c6d28f9b30fac13a082dfc4ab4918f573ed495b0
suvankar-mitra <suvankarmitra.me@gmail.com>
2019-04-19 23:45:03 +0530

    Added app properties file, some polishing of code

--- src/main/java/com/sameer/WordFinder/Main/Main.java
+++ src/main/java/com/sameer/WordFinder/Main/Main.java
@@ -1,35 +1,39 @@
 package com.sameer.WordFinder.Main;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 import com.sameer.WordFinder.Processor.WordProcessor;
 import com.sameer.WordFinder.ServiceGateway.OxfordServiceGateway;
+import com.sameer.WordFinder.utils.Util;
 
 public class Main {
 
 	public static void main(String args[]) {
-		
-		Scanner sc = new Scanner(System.in);
 		System.out.println("Input a String (MAXIMUM 4 LETTERS PLEASE !!!!!-)");
-		System.out.println("Permutaion of 5 and above letters makes more than 100 API calls which is not allowed by Oxford APIs");
-		System.out.println("for FREE accounts!!. Please enter the String -\n");
+		System.out.println("Permutation of 5 and above letters makes more than 100 API calls which is not allowed by Oxford APIs");
+		System.out.println("for FREE accounts!!");
+		System.out.print("Please enter the String: ");
+
+		Scanner sc = new Scanner(System.in);
 		String input = sc.next();
-		WordProcessor processor = new WordProcessor();
-		int startIndex = 0;
-		int endIndex = input.length()-1;
-		processor.permute(input, startIndex, endIndex);
-		List<String> permuteList = processor.getPermuteList();
-		OxfordServiceGateway dictionary  = new OxfordServiceGateway();
-		List<String>englishWordList = new ArrayList<String>();
-		for(String word: permuteList) {
-			if(dictionary.isEnglishWord(word)) {
-				englishWordList.add(word);
-			}
-		}
-		
-		
 		sc.close();
+
+        System.out.println("Checking all available English language words for the given alphabets...");
+
+		WordProcessor processor = new WordProcessor();
+        List<String> permuteList = processor.getListOfPermutedString(input);
+		OxfordServiceGateway dictionary  = new OxfordServiceGateway();
+        Map<String, List<String>> wordDefMap = dictionary.getDefinitionsOfWords(permuteList);
+
+        System.out.println();
+
+		for(Map.Entry<String, List<String>> wordSet :wordDefMap.entrySet()) {
+            System.out.println("Found: "+wordSet.getKey());
+            List<String> definitions = wordSet.getValue();
+            for(int i=0; i< definitions.size(); i++) {
+                System.out.println((i+1)+". "+ Util.capitalizeFirstLetter(definitions.get(i)));
+            }
+            System.out.println();
+        }
 	}
 }
--- src/main/java/com/sameer/WordFinder/ServiceGateway/OxfordServiceGateway.java
+++ src/main/java/com/sameer/WordFinder/ServiceGateway/OxfordServiceGateway.java
@@ -1,78 +1,102 @@
 package com.sameer.WordFinder.ServiceGateway;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONML;
-import org.json.JSONObject;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
 
 public class OxfordServiceGateway {
-	
-	private  String meaning;
-	
-	public OxfordServiceGateway() {
-		meaning = new String();
-	}
 
-	public  boolean isEnglishWord(String word) {
-		try {
-	    	String url  = "https://od-api.oxforddictionaries.com/api/v1/entries/en/";
-	        Client client = ClientBuilder.newClient(); 
-	         WebTarget target =  client.target(url+word);
-	         String str = target.request(MediaType.APPLICATION_JSON).header("app_id", "dc6d6104")
-	        		 .header("app_key", "615a27e265b2687e20a57427912c89f2").get(String.class);
+    public Map<String, List<String>> getDefinitionsOfWords(List<String> words) {
+        Map<String, List<String>> wordDefMap = new HashMap<>();
+        for(String word : words) {
+            List<String> definitions = isEnglishWord(word);
+            if (definitions != null) {
+                wordDefMap.put(word, definitions);
+            }
+        }
 
+        return wordDefMap;
+    }
 
-	         try {
-                 System.out.println("Found: "+word);
-             //    System.out.println(str);
-	        	 JSONObject jsonobj = new JSONObject(str);
-                 JSONArray resultsArray = jsonobj.getJSONArray("results");
-                 JSONObject resultsArrayFirstObject = resultsArray.getJSONObject(0);
-                 JSONArray lexicalEntriesArray = resultsArrayFirstObject.getJSONArray("lexicalEntries");
-                 for(int i=0;i<lexicalEntriesArray.length();i++){
-                     JSONObject lexicalEntriesObject = lexicalEntriesArray.getJSONObject(i);
-                     JSONArray entriesArray = lexicalEntriesObject.getJSONArray("entries");
-                     JSONObject entriesFirstObject = entriesArray.getJSONObject(0);
-                     JSONArray sensesArray = entriesFirstObject.getJSONArray("senses");
-                     for(int j=0;j<sensesArray.length();j++){
-                         JSONObject sensesFirstObject = sensesArray.getJSONObject(j);
-                         if(sensesFirstObject.has("definitions")){
-                             JSONArray definitionsArray = sensesFirstObject.getJSONArray("definitions");
-                             System.out.println(i+1+"."+j+". "+definitionsArray.getString(0));
-                         }
+    private List<String> isEnglishWord(String word) {
+        List<String> wordDefList = new ArrayList<>();
+        StringBuilder def;
 
-                     }
+        try {
+            Properties props = new Properties();
+            props.load(new FileInputStream(new File("app.properties")));
+
+            String url = props.getProperty("oxford_service_url");
+            String appId = props.getProperty("app_id");
+            String appKey = props.getProperty("app_key");
+
+            Client client = ClientBuilder.newClient();
+            WebTarget target = client.target(url + word);
+            String str = target.request(MediaType.APPLICATION_JSON).header("app_id", appId)
+                    .header("app_key", appKey).get(String.class);
 
 
-                    System.out.println();
-                 }
-
-
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-
-	   meaning = " ";
-	    	}
-	    	catch(NotFoundException e) {
-
-	    		return false;
-	    	}
-		return true;
-	}
-
-	public String getMeaning() {
-		return meaning;
-	}
-
-	public void setMeaning(String meaning) {
-		this.meaning = meaning;
-	}
+            try {
+                //System.out.println("Found: " + word);
+                //System.out.println(str);
+                JSONObject jsonObj = new JSONObject(str);
+                JSONArray resultsArray = jsonObj.getJSONArray("results");
+                JSONObject resultsArrayFirstObject = resultsArray.getJSONObject(0);
+                JSONArray lexicalEntriesArray = resultsArrayFirstObject.getJSONArray("lexicalEntries");
+                for (int i = 0; i < lexicalEntriesArray.length(); i++) {
+                    def = new StringBuilder();
+                    JSONObject lexicalEntriesObject = lexicalEntriesArray.getJSONObject(i);
+                    String lexicalCategory = lexicalEntriesObject.getString("lexicalCategory");
+                    def.append("(").append(lexicalCategory).append(") ");
+                    JSONArray entriesArray = lexicalEntriesObject.getJSONArray("entries");
+                    JSONObject entriesFirstObject = entriesArray.getJSONObject(0);
+                    JSONArray sensesArray = entriesFirstObject.getJSONArray("senses");
+                    for (int j = 0; j < sensesArray.length(); j++) {
+                        JSONObject sensesFirstObject = sensesArray.getJSONObject(j);
+                        if (sensesFirstObject.has("definitions")) {
+                            JSONArray definitionsArray = sensesFirstObject.getJSONArray("definitions");
+                            //System.out.println(i + 1 + "." + j + ". " + definitionsArray.getString(0));
+                            if (definitionsArray.length() > 0) {
+                                String lexicalDef = definitionsArray.getString(0);
+                                def.append(lexicalDef).append(" ");
+                                int removeIndex = -1;
+                                int defCount = 0;
+                                for(String defn : wordDefList) {
+                                    if(defn.contains(lexicalDef)) {
+                                        break;
+                                    }
+                                    if(def.toString().contains(defn)) {
+                                        removeIndex = defCount;
+                                        break;
+                                    }
+                                    defCount++;
+                                }
+                                if(removeIndex >= 0) {
+                                    wordDefList.remove(removeIndex);
+                                }
+                                wordDefList.add(def.toString());
+                            }
+                        }
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } catch (NotFoundException e) {
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return wordDefList;
+    }
 }
